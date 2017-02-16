a49b599a2ddef2f35b2338048f4e57adb89183a7
phit <phit@phit.link>
2017-02-16 15:02:22 +0100

    1.0.2

--- src/main/java/me/phit/gmb/GiveMeBiomes.java
+++ src/main/java/me/phit/gmb/GiveMeBiomes.java
@@ -20,7 +20,7 @@ public class GiveMeBiomes {
 
     public static final String MOD_ID = "givemebiomes";
     public static final String MOD_NAME = "GiveMeBiomes";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0.2";
 
     public static File savepath;
 
