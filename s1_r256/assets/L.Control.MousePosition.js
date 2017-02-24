cc669611062a2cd24606ee335a9dc0845fcf7650
phit <phit@phit.link>
2017-02-24 04:29:00 +0100

    example of 2.0.1

---
+++ s1_r256/assets/L.Control.MousePosition.js
@@ -0,0 +1,49 @@
+L.Control.MousePosition = L.Control.extend({
+  options: {
+    position: 'bottomleft',
+    separator: ' : ',
+    emptyString: 'Unavailable',
+    numDigits: 5,
+    prefix: ""
+  },
+
+  onAdd: function (map) {
+    this._container = L.DomUtil.create('div', 'leaflet-control-mouseposition');
+    L.DomEvent.disableClickPropagation(this._container);
+    map.on('mousemove', this._onMouseMove, this);
+    this._container.innerHTML=this.options.emptyString;
+    return this._container;
+  },
+
+  onRemove: function (map) {
+    map.off('mousemove', this._onMouseMove)
+  },
+
+  _onMouseMove: function (e) {
+    var lng = L.Util.formatNum(e.latlng.lng, this.options.numDigits);
+    var lat = L.Util.formatNum(e.latlng.lat, this.options.numDigits) * -1;
+    var value = 'X: ' + lng + ' Y: 64 Z: ' + lat;
+    var prefixAndValue = this.options.prefix + ' ' + value;
+    this._container.innerHTML = prefixAndValue;
+  }
+
+});
+
+L.Map.mergeOptions({
+    positionControl: false
+});
+
+L.Map.addInitHook(function () {
+    if (this.options.positionControl) {
+        this.positionControl = new L.Control.MousePosition();
+        this.addControl(this.positionControl);
+    }
+});
+
+L.control.mousePosition = function (options) {
+    return new L.Control.MousePosition(options);
+};
+
+
+//point.x = Math.floor((lng - 2 * lat)) / (24 * perPixel));
+//point.z = Math.floor((lng + 2 * lat) / (24 * perPixel));
